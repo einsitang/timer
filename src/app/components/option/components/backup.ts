9134e5f8263022f6fa5bd5b950d67ecbe55f4042
EinsiTang <my-tangjianbin@163.com>
2022-08-08 10:19:43 +0800

    尝试增加一个etag查看动态绑定效果，发现居然可以

--- src/app/components/option/components/backup.ts
+++ src/app/components/option/components/backup.ts
@@ -1,16 +1,26 @@
 import {defineComponent, h, PropType, ref, Ref, watch} from "vue";
 import {ElDivider, ElOption} from "element-plus/es";
 import {defaultAppearance, defaultBackup} from "@util/constant/option";
-import {ElSelect} from "element-plus";
+import {ElSelect, ElTag} from "element-plus";
 import {t} from "@app/locale";
 import {renderOptionItem} from "@app/components/option/common";
+import optionService from "@service/option-service";
+import {toggle} from "@util/dark-mode";
 
 const _default = defineComponent({
   name : "BackupOptionContainer",
-  setup(_props,_ctx){
+  setup(props,ctx){
     // const option: Ref<timer.option.BackupOption> = ref(defaultAppearance())
     const options: Ref<timer.option.BackupOption> = ref(defaultBackup())
+    console.log('value',options.value)
+    ctx.expose({
+      async reset() {
+        options.value = defaultBackup()
+        console.log('expose',options.value)
+      }
+    })
 
+    const etag = h(ElTag, { size: 'small' }, () => options.value.mode)
     const select = h(ElSelect, {
           modelValue: options.value.mode,
           size: 'small',
@@ -18,14 +28,14 @@ const _default = defineComponent({
           onChange: async (newVal: timer.option.BackupMode) => {
             const before = options.value.mode
             options.value.mode = newVal
-            console.log('onChange 生效',before,options.value.mode)
+            console.log('onChange 生效',before,options.value)
           }
         },{
           default :()=>['off','gist','google'].map(
               value => h(ElOption, { value, label: t(msg => msg.option.backup.options[value]) })
           )})
     const optionItem = renderOptionItem({
-          input: select
+          input: etag
         },
             msg => msg.backup.title,
         t(msg => msg.option.backup.options['off']))
@@ -35,6 +45,7 @@ const _default = defineComponent({
         optionItem,
       h(ElDivider),
       select,
+        etag
     ])
   }
 })
